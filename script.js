3e984547f947b575ec90f5ced0bd9252c64f1ba6
Lucas da Silva Oliveira <lucas-silva-98@hotmail.com>
2022-07-28 15:08:23 -0300

    att

---
+++ script.js
@@ -0,0 +1,28 @@
+let txt1 = document.getElementById('texto-interativo1')
+let txt2 = document.getElementById('texto-interativo2')
+let telaint1 = document.querySelector(".telainterativa1")
+let telaint2 = document.querySelector(".telainterativa2")
+
+function ligar() {
+    txt1.innerHTML = '<p>Seja bem vindo (a) !</p><p>Aperte "Next" para as próximas opções !</p>'
+    txt2.innerHTML = '<p>Seja bem vindo (a) !</p><p>Aperte "Next" para as próximas opções !</p>'
+    txt1.style.display = 'inline'
+    txt2.style.display = 'inline'
+    telaint1.style.backgroundColor = '#153c69'
+    telaint2.style.backgroundColor = '#153c69'
+}
+
+function desligar() {
+    txt1.innerHTML = ''
+    txt2.innerHTML = ''
+    txt1.style.display = 'none'
+    txt2.style.display = 'none'
+    telaint1.style.backgroundColor = 'black'
+    telaint2.style.backgroundColor = 'black'
+}
+
+document.getElementById('next1').addEventListener("click",next1);
+
+function next1() {
+    txt1.innerHTML= 'Esse é o display das minhas músicas favoritas !'    
+}
